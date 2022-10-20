0f243b5315527dbc84af2fec3c5ab802cd168fc8
erika fonseca <fonsecarika@gmail.com>
2022-10-20 23:31:44 +0300

    addtimetooffer

---
+++ db/migrate/20221020203010_add_time_to_offer.rb
@@ -0,0 +1,5 @@
+class AddTimeToOffer < ActiveRecord::Migration[7.0]
+  def change
+    add_column :offers, :time, :time
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_20_192912) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_20_203010) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_20_192912) do
     t.bigint "sub_cathegory_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.time "time"
     t.index ["sub_cathegory_id"], name: "index_offers_on_sub_cathegory_id"
   end
 
