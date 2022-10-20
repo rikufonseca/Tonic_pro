2941429284cada76f5583a622159badb372b4680
erika fonseca <fonsecarika@gmail.com>
2022-10-20 23:28:05 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
+  resources :clients, only: %i(index show new create edit update) do
+    resources :bookings, only: %i(new create)
+  end
+
+  resources :bookings, only: %i(index show edit update destroy)
+  resources :offers, only: %i(index show new create edit update destroy)
+  resources :cathegories, only: %i(index show new create edit update) do
+    resources :sub_cathegories, only: %i(index show new create edit update)
+  end
+
+  get 'dashboard', to: 'pages#dashboard'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
