16ebda8243d08734329ed7298840a22fdff076ec
erika fonseca <fonsecarika@gmail.com>
2022-10-21 12:48:42 +0300

    first page ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,16 @@
+puts "Destroy all datas..."
+puts "..."
+puts "..."
+
+User.destroy_all
+
+puts "All datas destroyed !"
+puts ""
+
+User.create!(email: "fonsecarika@gmail.com", password: "123456789")
+
+puts "Rika created !"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
