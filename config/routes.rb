68ad47ddc0b4e232b889bf7337693eb88a243ba4
erika fonseca <fonsecarika@gmail.com>
2022-10-21 11:57:48 +0300

    homepage front

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :clients, only: %i(index show new create edit update) do
-    resources :bookings, only: %i(new create)
-  end
-
-  resources :bookings, only: %i(index show edit update destroy)
+  resources :clients, only: %i(index show new create edit update)
+  resources :bookings, only: %i(index new create show edit update destroy)
   resources :offers, only: %i(index show new create edit update destroy)
   resources :cathegories, only: %i(index show new create edit update) do
     resources :sub_cathegories, only: %i(index show new create edit update)
