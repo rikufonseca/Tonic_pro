ac34ab4bd70284b6549a685187511a39fb7927b7
erika fonseca <fonsecarika@gmail.com>
2022-11-29 18:56:43 +0200

    ok

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,5 +3,5 @@ class Client < ApplicationRecord
 
   validates :name, :surname, :phone_number, presence: true
   validates :phone_number, uniqueness: true
-  validates :phone_number, length: { minimum: 10, too_short: "invalid number" }
+  validates :phone_number, length: { minimum: 10, too_short: "number too short" }, format: { with: / \d+ /, message: "allow only letter" }
 end
