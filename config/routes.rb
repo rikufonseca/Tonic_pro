8e1abd8bfad6d8c454ffbc27617c1608949769e3
Loris Reynaud <loris.reynaud@gmail.com>
2022-12-17 16:21:39 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,12 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   resources :clients, only: %i(index show new create edit update)
-  resources :bookings, only: %i(index new create show edit update destroy)
-  resources :categories, only: %i(index show new create edit update) do
-    resources :sub_categories, only: %i(index show new create edit update)
+  resources :bookings
+  resources :categories, only: %i(index new create edit update destroy) do
+    resources :sub_categories, only: %i(index new create edit update destroy)
   end
 
   get 'dashboard', to: 'pages#dashboard'
   post 'getclient', to: 'bookings#getclient'
   post 'getsubcat', to: 'bookings#getsubcat'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
