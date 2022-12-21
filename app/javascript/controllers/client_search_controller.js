55a4551146fb38647709acf2063c9e52165a1b3a
Loris Reynaud <loris.reynaud@gmail.com>
2022-12-21 18:18:12 +0100

    phone number must be 10 and integer

--- app/javascript/controllers/client_search_controller.js
+++ app/javascript/controllers/client_search_controller.js
@@ -1,41 +1,57 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["form", "phone", "clientname", "clientsurname", "category", "errormessage", "catDiv"]
+  static targets = ["form", "phone", "clientname", "clientsurname", "category", "catDiv", "submit", "phoneInput"]
+
+  connect() {
+    const submit = this.submitTarget;
+    submit.disabled = true;
+  }
 
   search(event) {
     event.preventDefault()
 
-    const url = `http://${window.location.host}/getclient`
-    const client_name = this.clientnameTarget
-    const client_surname = this.clientsurnameTarget
-    const phone = this.phoneTarget.value
-    let errormessage = this.errormessageTarget
+    const url = `http://${window.location.host}/getclient`;
+    const client_name = this.clientnameTarget;
+    const client_surname = this.clientsurnameTarget;
+    const phone = this.phoneTarget.value;
+    const submit = this.submitTarget;
+    const phoneInput = this.phoneInputTarget; 
 
+    console.log(client_name.value === "")
+
+    phoneInput.innerHTML = "";
+
+    if(phone.length === 10 && Number.isInteger(parseInt(phone))) {
       fetch(url, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ phone: phone })
       })
-        .then(response => response.json())
-        .then((data) => {
-          appendData(data)
-        })
+      .then(response => response.json())
+      .then((data) => {
+        appendData(data)
+      })
 
-    function appendData(data) {
-      let div = document.createElement("div");
-      let div_two = document.createElement("div_two");
-      if ((data.length === 0)) {
-        div = "";
-        div_two = "";
-      } else {
-        for (let i = 0; i < data.length; i++) {
-          div = data[i].name;
-          div_two = data[i].surname;
+      function appendData(data) {
+        let div = document.createElement("div");
+        let div_two = document.createElement("div_two");
+        if ((data.length === 0)) {
+          div = "";
+          div_two = "";
+        } else {
+          for (let i = 0; i < data.length; i++) {
+            div = data[i].name;
+            div_two = data[i].surname;
+          }
         }
+        client_name.value = div;
+        client_surname.value = div_two;
       }
-      client_name.value = div;
-      client_surname.value = div_two;
+      submit.disabled = false;
+    } else {
+      phoneInput.insertAdjacentHTML('beforeend', 'Phone number is invalid');
+      submit.disabled = true;
     }
   }
 
@@ -47,9 +63,6 @@ export default class extends Controller {
     const category = categories[categories.length -1].value;
 
     const catDivs = this.catDivTargets;
-
-  //  console.log(category)
-  //  console.log(JSON.stringify({ category: category }))
 
     fetch(url, {
       method: "POST",
