2c9197dbc1801f5bd8ac9e5af3dded444441c369
Loris Reynaud <loris.reynaud@gmail.com>
2023-02-13 13:44:35 +0100

    wip

--- app/controllers/plannings_controller.rb
+++ app/controllers/plannings_controller.rb
@@ -15,7 +15,7 @@ class PlanningsController < ApplicationController
     shifts = @planning.shifts
     time = Time.new(@planning.from.strftime("%Y"), @planning.from.strftime("%m"), @planning.from.strftime("%d"), 10, 00)
     day = Date.new(@planning.to.strftime("%Y").to_i, @planning.from.strftime("%m").to_i, @planning.from.strftime("%d").to_i)
-
+    
     @monday_shifts = shifts.filter { |shift| shift.beginning.monday? }
     @tuesday_shifts = shifts.filter { |shift| shift.beginning.tuesday? }
     @wednesday_shifts = shifts.filter { |shift| shift.beginning.wednesday? }
@@ -23,6 +23,9 @@ class PlanningsController < ApplicationController
     @friday_shifts = shifts.filter { |shift| shift.beginning.friday? }
     @saturday_shifts = shifts.filter { |shift| shift.beginning.saturday? }
     @sunday_shifts = shifts.filter { |shift| shift.beginning.sunday? }
+
+    @week_shifts = []
+    @week_shifts.push(@monday_shifts, @tuesday_shifts, @wednesday_shifts, @thursday_shifts, @friday_shifts, @saturday_shifts)
   end
 
   def new
